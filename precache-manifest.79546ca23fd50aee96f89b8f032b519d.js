1f08c240a05da8a7b5549673bbe960f92398eebb
Le Van Quoc Viet <61400407+lvqviet@users.noreply.github.com>
2020-10-11 18:13:02 +0700

    Updates

---
+++ precache-manifest.79546ca23fd50aee96f89b8f032b519d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5519cfda38c41d3f9ee6397b42d0327",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "1391a47eb8d588a59597",
+    "url": "/quote-generator/static/css/main.8bb5b266.chunk.css"
+  },
+  {
+    "revision": "392bb08e5730666b4718",
+    "url": "/quote-generator/static/js/2.64347edf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quote-generator/static/js/2.64347edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1391a47eb8d588a59597",
+    "url": "/quote-generator/static/js/main.603ca9fc.chunk.js"
+  },
+  {
+    "revision": "95f6f4bf04c9c9655887",
+    "url": "/quote-generator/static/js/runtime-main.f5556377.js"
+  }
+]);
