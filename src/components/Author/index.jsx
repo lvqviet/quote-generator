ffe2573be303970eadb6deb019070ead133ded06
Le Van Quoc Viet <61400407+lvqviet@users.noreply.github.com>
2020-10-11 16:54:38 +0700

    dracula theme

--- src/components/Author/index.jsx
+++ src/components/Author/index.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import './author.scss';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
-function Author({ author, genre, onClick }) {
+function Author({ author, genre, theme, onClick }) {
   return (
-    <div className="author" onClick={onClick}>
+    <div 
+    className="author" 
+    onClick={onClick}
+    style={{
+      color: theme ? '' : '#ff5555',
+    }}
+    >
       <div>
-        <h3>{author}</h3>
+        <h3 style={{color: theme ? '' : '#ff5555'}}>
+          {author}
+        </h3>
         <p>{genre}</p>
       </div>
-      <ArrowRightAltIcon
+      
+        <ArrowRightAltIcon
         fontSize="large"
-        style={{ color: 'white' }}
+        style={{ color: theme ? 'white' : '#ff5555'}}
       />
+      
+      
     </div>
   );
 }
--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -6,11 +6,14 @@ import Footer from '../Footer';
 import Loading from '../Loading';
 import Quote from '../Quote';
 import LoopIcon from '@material-ui/icons/Loop';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import './randomQuote.scss';
 
 function RandomQuote() {
   const [quotes, setQuote] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [theme, setTheme] =useState(true);
 
   const getData = async (url, array) => {
     try {
@@ -18,7 +21,6 @@ function RandomQuote() {
       if (array) {
         const res = await Axios.get(url);
         setQuote(res.data.quotes);
-        console.log(res.data.quotes);
       } else {
         const res = await Axios.get(url);
         setQuote(res.data.quote);
@@ -44,6 +46,13 @@ function RandomQuote() {
     );
     return;
   };
+
+  const handleTheme = () => {
+    
+    setTheme(!theme);
+    
+  }
+
   useEffect(getQuote, []);
 
   return (
@@ -52,30 +61,49 @@ function RandomQuote() {
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
+        backgroundColor: theme ? '': '#282a36' ,
+        transition: '150ms'
       }}
     >
-      <div className="btn">
+      <div className="btn" style={{color: theme ? '' : '#ff5555'}}>
+        <div className="btn--theme" onClick={handleTheme}>
+          { theme ? (
+            <WbSunnyIcon fontSize='large'/>
+          ) : (
+              <Brightness2Icon fontSize='large' />
+          )}
+        </div>
         <div className="btn--random" onClick={getQuote}>
           random <LoopIcon />
         </div>
       </div>
       {loading ? (
-        <Loading />
+        <div style={{color: theme ? '' : '#ff5555'}}> 
+          <Loading />
+        </div>
+        
       ) : (
         <div className="container">
           {quotes.length ? (
-            <>
-              <h1>{quotes[0].quoteAuthor}</h1>
+            <div style={{color: theme ? '' : '#ff5555'}}>
+              <h1 >{quotes[0].quoteAuthor}</h1>
               {quotes.map(quote => (
-                <Quote key={quote._id} quote={quote.quoteText} />
+                <Quote 
+                key={quote._id} 
+                quote={quote.quoteText} 
+                />
               ))}
-            </>
+            </div>
           ) : (
-            <div className="content">
+            <div 
+            className="content"
+            style={{color: theme ? '' : '#ff5555'}}
+            >
               <Quote quote={quotes.quoteText} />
               <Author
                 author={quotes.quoteAuthor}
                 genre={quotes.quoteGenre}
+                theme={theme}
                 onClick={getQuotesByAuthor}
               />
             </div>
